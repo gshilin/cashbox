6c6d8a397b18c649e181f6a5159578fa58cf8608
Gregory Shilin <gshilin@gmail.com>
2017-02-12 12:09:27 +0200

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-(1..9).each do |d|
-  puts "Desk #{d}"
-  desk = Cashdesk.find(d)
-  (1..100).each do |s|
-    shift = Shift.find_by(id: d)
-    unless shift
-      shift = Shift.create(state: 'inactive')
-      desk.shifts << shift
-    end
-    (1..100).each do |x|
-      case x % 4
-        when 0
-          income = Income.create(amount: rand(200), kind: 'cc', currency: 'ILS', success: true)
-          shift.incomes << income
-        when 1
-          income = Income.create(amount: rand(200), kind: 'nis', currency: 'ILS', success: true)
-          shift.incomes << income
-        when 2
-          income = Income.create(amount: rand(200), kind: 'usd', currency: 'USD', success: true)
-          shift.incomes << income
-        when 3
-          income = Income.create(amount: rand(200), kind: 'eur', currency: 'EUR', success: true)
-          shift.incomes << income
-      end
-    end
-  end
-end
